1157d3d1af276e39a25b9fd75411710206659c51
xFadedcyclone <116615801+xFadedcyclone@users.noreply.github.com>
2022-12-03 19:18:50 -0700

    Add files via upload

---
+++ dice.js
@@ -0,0 +1,9 @@
+let random11 = Math.floor(Math.random() * 6 + 1)
+let random22 = Math.floor(Math.random() * 6 + 1)
+document.querySelector('.img1').setAttribute('src', '/Dicee Challenge - Starting Files/images/dice'+ random11 +'.png')
+    
+
+
+
+
+document.querySelector('.img2').setAttribute('src', '/Dicee Challenge - Starting Files/images/dice' + random22 + '.png')
